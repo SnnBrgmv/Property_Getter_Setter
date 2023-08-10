9226e7944ed7c68dfb8c84855ae875db1bdf5031
SnnBrgmv <sananai@code.edu.az>
2023-08-10 16:12:13 +0400

    C# property set and get methods

---
+++ Property_Set_Get/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Property_Set_Get
+{
+    public class SimCard
+    {
+        public string CarrierName { get; set; }
+        public string Number { get; set; }
+        public decimal Balance { get; set; }
+        public decimal Tariff { get; set; }
+
+        public void IncreaseBalance(decimal amount)
+        {
+            if (amount <= 0) return;
+            Balance += amount;
+        }
+        public void Call()
+        {
+            if (Tariff > Balance) {
+                Console.WriteLine($"Not enough money Balance: {Balance = 0}");
+            }
+            else
+            {
+                Balance -= Tariff;
+                Console.WriteLine($"Balance: {Balance}");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimCard simCard = new SimCard()
+            {
+                CarrierName = "Azercell",
+                Number = "050-243-00-73",
+                Balance = 0.01m,
+                Tariff = .05m
+            };
+
+            //simCard.IncreaseBalance(.2m);
+            simCard.Call();
+        }
+    }
+}
